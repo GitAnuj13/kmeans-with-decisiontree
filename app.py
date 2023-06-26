ee200569738a8ccf85fe740bb902fd2c25bcd4fd
Anuj Upadhyay <134092817+GitAnuj13@users.noreply.github.com>
2023-06-26 17:20:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import pandas as pd
 import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pickle
+import joblib
 
 filename = 'final_model.sav'
-loaded_model = pickle.load(open(filename, 'rb'))
+loaded_model = joblib.load(filename)
 df = pd.read_csv("Clustered_Customer_Data.csv")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
